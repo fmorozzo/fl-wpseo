453e595ddcfb541cb1b1a96ebf470e4070badf5b
Federico Morozzo della Rocca <fmorozzo@gmail.com>
2018-10-26 15:19:51 +0200

    with theme facility lockers (base)

---
+++ wp-content/themes/fl/footer.php
@@ -0,0 +1,15 @@
+<?php if (get_theme_mod('devdmbootstrap4_show_credit_setting', 1)) : ?>
+
+    <div class="container dmbs-author-credits">
+        <div class="row">
+            <div class="col-12">
+                <p><a href="<?php echo esc_url(devdmbootstrap4_credit_link()); ?>"><?php esc_html_e('DevDmBootstrap4 Created by Danny Machal (DevDm.com)','devdmbootstrap4'); ?></a></p>
+            </div>
+        </div>
+    </div>
+
+<?php endif; ?>
+
+<?php wp_footer(); ?>
+</body>
+</html>
